51ba9d8714c3863ab57c651b48bf118411399f2b
mjolewis <mjolewis@gmail.com>
2019-08-18 11:24:57 -0400

    Power of Two Solution

--- Power of Two/src/PowerOfTwo.java
+++ Power of Two/src/PowerOfTwo.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Given an integer, write a function to determine if it is a power of two.
  *
@@ -18,15 +21,13 @@
  */
 public class PowerOfTwo {
     public boolean isPowerOfTwo(int n) {
-        if (n == 1 || n == 4) {
-            return true;
-        } else {
-            return (n % 4 == 0);
-        }
+        if (n == 0) return false;
+        while (n % 2 == 0) n /= 2;
+        return n == 1; // 2^1 is true, but every other result is captured in the while loop
     }
 
     public static void main(String[] args) {
         PowerOfTwo powerOfTwo = new PowerOfTwo();
-        System.out.println(powerOfTwo.isPowerOfTwo(4));
+        System.out.println(powerOfTwo.isPowerOfTwo(536870912));
     }
 }
--- Power of Two/src/PowerOfTwoTest.java
+++ Power of Two/src/PowerOfTwoTest.java
@@ -8,11 +8,21 @@ public class PowerOfTwoTest {
         PowerOfTwo powerOfTwo2 = new PowerOfTwo();
         PowerOfTwo powerOfTwo3 = new PowerOfTwo();
         PowerOfTwo powerOfTwo4 = new PowerOfTwo();
+        PowerOfTwo powerOfTwo5 = new PowerOfTwo();
+        PowerOfTwo powerOfTwo6 = new PowerOfTwo();
+        PowerOfTwo powerOfTwo7 = new PowerOfTwo();
+        PowerOfTwo powerOfTwo8 = new PowerOfTwo();
+        PowerOfTwo powerOfTwo9 = new PowerOfTwo();
 
-        assertEquals(true, powerOfTwo1.isPowerOfTwo(0));
-        assertEquals(true, powerOfTwo2.isPowerOfTwo(4));
-        assertEquals(false, powerOfTwo3.isPowerOfTwo(218));
-        assertEquals(true, powerOfTwo3.isPowerOfTwo(1));
+        //assertEquals(false, powerOfTwo1.isPowerOfTwo(0));
+        assertEquals(true, powerOfTwo2.isPowerOfTwo(1));
+        assertEquals(true, powerOfTwo3.isPowerOfTwo(2));
+        assertEquals(true, powerOfTwo4.isPowerOfTwo(4));
+        assertEquals(true, powerOfTwo5.isPowerOfTwo(8));
+        assertEquals(true, powerOfTwo6.isPowerOfTwo(16));
+        assertEquals(true, powerOfTwo7.isPowerOfTwo(32));
+        assertEquals(true, powerOfTwo8.isPowerOfTwo(64));
+        assertEquals(true, powerOfTwo9.isPowerOfTwo(536870912));
 
     }
 }
