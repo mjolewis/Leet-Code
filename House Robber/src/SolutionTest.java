c0797a64f1133158673d5bc88930e6c00b4b1b2f
mjolewis <mjolewis@gmail.com>
2019-08-21 20:31:30 -0400

    House Robber Solution using Dynamic Programming

---
+++ House Robber/src/SolutionTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest {
+    @Test
+    public void soltuionTest() {
+        Solution solution1 = new Solution();
+        Solution solution2 = new Solution();
+
+        //assertEquals(4, solution1.rob(new int[] {1,2,3,1}));
+        //assertEquals(12, solution1.rob(new int[] {2,7,9,3,1}));
+    }
+}
