a0271da8af4e8338cb8dc94d3d706bb2a6acb4d2
mjolewis <mjolewis@gmail.com>
2019-08-22 15:57:49 -0400

    Binary Search Solution

--- Binary Search/src/Solution.java
+++ Binary Search/src/Solution.java
@@ -19,9 +19,10 @@ public class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
+        int mid;
 
         while (start <= end) {
-            int mid = start + (end - start) / 2;
+            mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
