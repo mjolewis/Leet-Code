a08912311ba3e56520c680143590480b569a059d
Michael Lewis <mjolewis@gmail.com>
2019-09-12 10:10:01 -0400

    Valid Anagram Solution using Map

--- Valid Anagram/src/Solution.java
+++ Valid Anagram/src/Solution.java
@@ -1,2 +1,48 @@
-public class isAnagram] {
+/*
+Author: Michael Lewis
+Date: 9/11/19
+ */
+
+import java.util.*;
+
+/**
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ */
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i) - 'a', map.getOrDefault(s.charAt(i) - 'a', 0) + 1);
+            map.put(t.charAt(i) - 'a', map.getOrDefault(t.charAt(i) - 'a', 0) - 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i) - 'a') != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.isAnagram("anagram", "nagaram");
+    }
 }
--- Valid Anagram/test/SolutionTest.java
+++ Valid Anagram/test/SolutionTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
-
+    @Test
+    public void solutionTest() {
+        Solution solution = new Solution();
+        assertTrue(solution.isAnagram("anagram", "nagaram"));
+        assertFalse(solution.isAnagram("rat", "car"));
+        assertFalse(solution.isAnagram("a", "ab"));
+        assertFalse(solution.isAnagram("ac", "bb"));
+        assertFalse(solution.isAnagram("fe", "ja"));
+    }
 }
